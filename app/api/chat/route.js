2f1d3e19fc8bfc654354825ee2ba094872cb8802
larae6 <laralara359@icloud.com>
2024-08-06 22:48:48 -0700

    step 5

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,5 +1,8 @@
-import { NextResponse } from "next/server";
+import {NextResponse} from 'next/server' // Import NextResponse from Next.js for handling responses
+import OpenAI from 'openai' // Import OpenAI library for interacting with the OpenAI API
+
 export async function POST(req){
+    const openai = new OpenAI();
     console.log('POST /api/chat')
     const data = await req.json()
     console.log(data)
